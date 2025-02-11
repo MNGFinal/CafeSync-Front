adc0a2ca4c7a20342eb96342b31bdf137e1d4ade
MelodyYD <epyneoz27@gmail.com>
2025-02-11 18:20:57 +0900

    [25.02.11] 가맹점 스케줄 관리 UI 구현

--- cafe-sync/src/fran/pages/employee/Schedule.js
+++ cafe-sync/src/fran/pages/employee/Schedule.js
@@ -1,8 +1,13 @@
+import MyCalendar from "./FullCalendar";
+
 function Schedule() {
   return (
     <>
       <div className="page-header">
         <h3>스케줄 조회</h3>
+      </div>
+      <div>
+        <MyCalendar/>
       </div>
     </>
   );
