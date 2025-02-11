825b6c12d6e2a7b5dc294ad7815f37e0ef0eb228
MelodyYD <epyneoz27@gmail.com>
2025-02-11 10:14:30 +0900

    [25.02.11] 본사 메인 페이지 UI 구현

--- cafe-sync/src/hq/pages/main/HQMain.js
+++ cafe-sync/src/hq/pages/main/HQMain.js
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import st from "./HQMain.module.css";
+
 function HQMain() {
   return (
     <>
-      <h1>본사 메인 화면</h1>
+      {/* <h1>본사 메인 화면</h1> */}
+      <div className={`noticeSection ${st.sec}`}>
+        <Link to="/hq/notice">공지사항</Link>
+        <hr/>
+      </div>
+      <div className={`planSection ${st.sec}`}>
+        <Link to="/hq/plan">이벤트 일정</Link>
+        <hr/>
+      </div>
+      <div className={`statsSection ${st.sec}`}>
+        <Link to="/hq/stats">우수 가맹점 매출 현황 그래프</Link>
+        <hr/>
+      </div>
     </>
   );
 }
