fd6dfc2f6246cd343badb5f3e4bf91b1eea30295
JungJay <keunhee0920@gmail.com>
2025-02-12 10:51:31 +0900

    [25.02.12] App.js 코드 일부 수정

--- cafe-sync/src/App.js
+++ cafe-sync/src/App.js
@@ -74,27 +74,6 @@ function App() {
           <Route path="find-pass/modify" element={<PassModify />} />
         </Route>
 
-
-        {/* ✅ 가맹점 라우트 */}
-        <Route path="/fran" element={<Layout />}>
-          <Route index element={<Main />} />
-          <Route path="inventory" element={<Inventory />} />
-          <Route path="fran-inventory" element={<FranInventory />} />
-          <Route path="orders" element={<Orders />} />
-          <Route path="menus" element={<Menus />} />
-          <Route path="slip" element={<Slip />} />
-          <Route path="duty" element={<Duty />} />
-          <Route path="income" element={<Income />} />
-          <Route path="complain" element={<Complain />} />
-          <Route path="employee" element={<Employee />} />
-          <Route path="schedule" element={<Schedule />} />
-          <Route path="dayoff" element={<DayOff />} />
-          <Route path="notice" element={<Notice />} />
-          <Route path="notice/regist" element={<NoticeRegist/>} />
-          <Route path="chat" element={<Chat />} />
-          <Route path="barista-note" element={<BaristaNote />} />
-          <Route path="stats" element={<Stats />} />
-
         {/* ✅ 보호된 가맹점 라우트 */}
         <Route element={<ProtectedRoute />}>
           <Route path="/fran" element={<Layout />}>
