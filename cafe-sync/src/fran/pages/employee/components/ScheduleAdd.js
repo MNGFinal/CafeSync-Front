1e88d1086cdb555921b9f0f6abb566db07bb708d
MelodyYD <epyneoz27@gmail.com>
2025-02-17 18:23:26 +0900

    [25.02.17] 스케줄 등록 UI 구현

--- cafe-sync/src/fran/pages/employee/components/ScheduleAdd.js
+++ cafe-sync/src/fran/pages/employee/components/ScheduleAdd.js
@@ -1,11 +1,17 @@
-import {useState} from "react";
-import Modal from "/src/components/Modal";
+import { useState } from "react";
+import Modal from "../../../../components/Modal";
+import modalStyle from "../../../../components/ModalButton.module.css";
+import style from "../styles/schedule.module.css"
 
 const ScheduleAdd = ({isModalOpen, setIsModalOpen}) => {
-    // const [isModalOpen, setIsModalOpen] = useState(false);      // 모달 여닫기 관리
     const [workers, setWorkers] = useState([
-        { worker: '', division: '', key: Date.now()},
+        { worker: '', division: '', key: Date.now() },
     ]);
+    const closeHandler = () => {
+        setIsModalOpen(false);
+        setWorkers([{ worker: '', division: '', key: Date.now() }]);
+    };
+    const confirmHandler = null;
 
     // 근로자 추가
     const addWorkerHandler = () => {
@@ -30,37 +36,49 @@ const ScheduleAdd = ({isModalOpen, setIsModalOpen}) => {
 
     return (
         <div>
-            <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-                <h2>스케줄 신규 등록</h2>
-                <hr/>
-                <span>날짜</span>
-                <input type="date"/>
-                <h3>근로자 추가</h3>
+            <Modal 
+                isOpen={isModalOpen} 
+                onClose={closeHandler}
+                buttons={[
+                    { text: "등록", onClick: confirmHandler, className: modalStyle.confirmButtonB },
+                    { text: "취소", onClick: closeHandler, className: modalStyle.cancelButtonB }
+                ]}
+            >
+                <div className={style.scheduleAddContainer}>
+                    <h2>스케줄 신규 등록</h2>
+                    <hr/>
+                    <div className={style.scheduleAddBody}>
+                        <span style={{ marginRight: "36px" }} className={style.span}>날짜</span>
+                        <input type="date"/>
+                        <h3>근로자 추가</h3>
 
-                {workers.map(({ worker, division, key }, index) => (
-                    <div key={key} style={{ display: "flex", alignItems: "center" }}>
-                        <select
-                            name = "worker"
-                            value={worker}
-                            onChange={(e) => workerChangeHandler(e, key)}
-                        >
-                            <option value="">근로자 선택</option>
-                            {/* 옵션이 이곳에 동적으로 추가되도록 하기 */}
-                        </select>
-                        <span>근로 시간</span>
-                        <input
-                            type="number"
-                            name="division"
-                            value={division}
-                            onChange={(e) => workerChangeHandler(e, key)}
-                            placeholder="근로 시간"
-                        />
-                        <button onClick={addWorkerHandler}>+</button>
-                        {index > 0 && (
-                            <button onClick={rmWorkerHandler}>-</button>
-                        )}
+                        {workers.map(({ worker, division, key }, index) => (
+                            <div key={key} className={style.workers}>
+                                <span className={style.span}>근로자</span>
+                                <select
+                                    name = "worker"
+                                    value={worker}
+                                    onChange={(e) => workerChangeHandler(e, key)}
+                                >
+                                    <option value="">근로자 선택</option>
+                                    {/* 옵션이 이곳에 동적으로 추가되도록 하기 */}
+                                </select>
+                                <span style={{ marginLeft: "50px" }}  className={style.span}>근로 시간</span>
+                                <input
+                                    type="number"
+                                    name="division"
+                                    value={division}
+                                    onChange={(e) => workerChangeHandler(e, key)}
+                                    placeholder="근로 시간"
+                                />
+                                <button onClick={addWorkerHandler} className={style.addWorkerBtn}>+</button>
+                                {index > 0 && (
+                                    <button onClick={rmWorkerHandler} className={style.delWorkerBtn}>─</button>
+                                )}
+                            </div>
+                        ))}
                     </div>
-                ))}
+                </div>
             </Modal>
         </div>
     )
