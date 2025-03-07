1609ffd2bdaab7955a30dbe4c79f19ee214ea67b
DESKTOP-HGTQQTV\PC <keunhee0920@gmail.com>
2025-03-08 08:06:01 +0900

    [25.03.08] 본사 채팅 기능 추가

--- cafe-sync/src/hq/pages/chat/HQChat.js
+++ cafe-sync/src/hq/pages/chat/HQChat.js
@@ -1,11 +1,27 @@
-function HQChat() {
+import styles from "./HQChat.module.css";
+import HQEmployee from "./HQEmployee";
+import HQRoom from "./HQRoom";
+import HQChatting from "./HQChatting";
+
+function Chat() {
   return (
     <>
       <div className="page-header">
         <h3>실시간 채팅</h3>
       </div>
+      <div className={styles.container}>
+        <div className={styles.empBox}>
+          <HQEmployee />
+        </div>
+        <div className={styles.roomBox}>
+          <HQRoom />
+        </div>
+        <div className={styles.chatBox}>
+          <HQChatting />
+        </div>
+      </div>
     </>
   );
 }
 
-export default HQChat;
+export default Chat;
