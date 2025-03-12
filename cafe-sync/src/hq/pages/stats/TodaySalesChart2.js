42409b66c9b38a4b617d88e10e75261f68656bee
JungJay <keunhee0920@gmail.com>
2025-03-12 11:27:19 +0900

    [25.03.12] 컴포넌트 수정

---
+++ cafe-sync/src/hq/pages/stats/TodaySalesChart2.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import StatChart from "./StatChart";
+
+const TodaySalesChart2 = ({ searchTrigger }) => {
+    const [todaySalesData, setTodaySalesData] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/hq/today-sales?today=2025-03-06")
+            .then(response => {
+                console.log("📌 가맹점 매출 데이터:", response.data);
+                setTodaySalesData(response.data);
+            })
+            .catch(error => console.error("Error fetching today's sales:", error));
+    }, [searchTrigger]); // ✅ 조회 버튼을 눌렀을 때만 API 호출
+
+    return <StatChart title="가맹점 오늘의 매출 순위" data={todaySalesData} type="store" />;
+};
+
+export default TodaySalesChart2;
